f254913f0f54510b707605defed0f12596f9a845
Sudhakar-16-2005 <sudhasujith2005@gmail.com>
2025-06-23 15:00:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Pattern 6/pattern-6.java
+++ Difficulty: Basic/Pattern 6/pattern-6.java
@@ -2,7 +2,7 @@ class Solution {
 
     void printTriangle(int n) {
         // code here
-          for (int i=1;i<n;i++){
+          for (int i=0;i<n;i++){
             for (int j=1;j<=n-i;j++){
                 System.out.print(j+" ");
             }
