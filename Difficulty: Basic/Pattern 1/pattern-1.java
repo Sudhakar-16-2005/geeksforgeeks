6a075147b3db6182d4fb59e7a72d2ff41043b975
Sudhakar-16-2005 <sudhasujith2005@gmail.com>
2025-06-23 12:39:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 1/pattern-1.java
@@ -0,0 +1,12 @@
+class Solution {
+
+    void printSquare(int n) {
+        // code here
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=n;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
