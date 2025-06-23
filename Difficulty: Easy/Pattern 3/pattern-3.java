3cee2cc9473d8c6a9b5a26ec2ed090f8186060c7
Sudhakar-16-2005 <sudhasujith2005@gmail.com>
2025-06-23 13:11:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Pattern 3/pattern-3.java
+++ Difficulty: Easy/Pattern 3/pattern-3.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(j+" ");
-            }
+            }    
             System.out.println();
         }
     }
