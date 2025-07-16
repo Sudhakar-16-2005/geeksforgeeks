0ca6be0cd9a44977a3e122ba580003ce546f8c3d
Sudhakar-16-2005 <sudhasujith2005@gmail.com>
2025-07-16 11:34:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Convert array into Zig-Zag fashion/convert-array-into-zig-zag-fashion.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public static void zigZag(int[] arr) {
+        // code here
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (i % 2 == 0) {
+                if (arr[i] > arr[i + 1]) {
+                    
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            } else {
+                if (arr[i] < arr[i + 1]) {
+                   
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
