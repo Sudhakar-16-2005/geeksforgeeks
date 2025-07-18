827924b4028fba8be6d7c7931b08cfd8dbe0368d
Sudhakar-16-2005 <sudhasujith2005@gmail.com>
2025-07-18 10:22:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.java
@@ -0,0 +1,23 @@
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    int getMiddle(Node head) {
+        // Your code here.
+        Node slow=head;
+        Node fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        if(fast.next!=null){
+            return slow.next.data;
+        }
+        return slow.data;
+    }
+}
